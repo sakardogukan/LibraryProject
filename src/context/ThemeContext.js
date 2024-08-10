1b91b21974a5ede14d5f7bce984cc843ba30044b
DGKN <sakardogukan@gmail.com>
2024-08-10 20:29:41 +0300

    ThemeContext file updated

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,1 +1,25 @@
 // Theme Context
+
+import { createContext, useContext, useState } from "react"
+
+//! 1- Theme Context oluşturulması:
+const ThemeContext = createContext()
+
+//! 2- Sarmalayıcı (Provider) Componenet:
+const ThemeContextProvider = ({ children }) => {
+
+    // local State
+    const [myTheme, setMyTheme] = useState("light")
+    const values = { myTheme, setMyTheme }
+
+    return (
+        <ThemeContext.Provider value={values}>{children}</ThemeContext.Provider>
+    )
+}
+
+//! 3- Consuming custom hook:
+export const useThemeContext = () => {
+    return useContext(ThemeContext)
+}
+
+export default ThemeContextProvider
