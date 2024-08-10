54ff056ec2dc077cd88a4a567f3344434f0c39ff
DGKN <sakardogukan@gmail.com>
2024-08-10 20:31:12 +0300

    app.js file updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { ThemeProvider } from "styled-components"
+import { lightTheme, darkTheme } from "./styles/theme"
+import AppRouter from "./router/AppRouter"
+import { GlobalStyles } from "./styles/Global.styles"
+import { useThemeContext } from "./context/ThemeContext"
+
+
 function App() {
+  const { myTheme } = useThemeContext()
+  const themes = myTheme === "light" ? lightTheme : darkTheme
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <ThemeProvider theme={themes}>
+      <GlobalStyles />
+      <AppRouter />
+    </ThemeProvider>
   )
 }
 
