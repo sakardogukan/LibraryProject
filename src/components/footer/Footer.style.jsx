5d000c8c102ecf3133884c06e36b9cd8f349cf45
DGKN <sakardogukan@gmail.com>
2024-08-10 20:29:01 +0300

    Footer.style.jsx file updated

--- src/components/footer/Footer.style.jsx
+++ src/components/footer/Footer.style.jsx
@@ -1,1 +1,17 @@
 // footer Style
+
+import styled from "styled-components"
+
+export const FooterContainer = styled.div`
+  position: relative;
+`
+
+export const FooterBtn = styled.button`
+  position: fixed;
+  border: none;
+  outline: none;
+  background: none;
+  cursor: pointer;
+  bottom: 0;
+  padding: 1rem;
+`
