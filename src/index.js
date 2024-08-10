cfe1963d89e2c2e8f5be5e8584e449eb7039ad2f
DGKN <sakardogukan@gmail.com>
2024-08-10 20:31:24 +0300

    index.js file updated

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import ThemeContextProvider from "./context/ThemeContext"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
+  <ThemeContextProvider>
     <App />
+  </ThemeContextProvider>
 )
