e1841c0c5a8a2372a2663ce6beced56b1e6a06e4
DGKN <sakardogukan@gmail.com>
2024-08-11 19:18:41 +0300

    login file updated

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React from "react"
+import { useAuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import {
+  FormContainer,
+  LoginContainer,
+  Header,
+  StyledForm,
+  StyledButton,
+  StyledInput,
+} from "./Login.style"
 
 const Login = () => {
+
+  const { setUser } = useAuthContext();
+  const navigate = useNavigate();
+  // const [username, setUserName] = useState("");
+  // const [password, setPassword] = useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUser(true);
+    navigate(-1);
+    // if (
+    //   username.toLowerCase() === "admin" &&
+    //   password.toLowerCase() === "12345"
+    // ) {
+    //   setUser(true);
+    //   navigate(-1);
+    // } else {
+    //   alert("Username ve Password bilgisini doğru giriniz.");
+    // }
+  };
+
   return (
-    <div>Login</div>
+    <LoginContainer>
+      <FormContainer>
+        <StyledForm onSubmit={handleSubmit}>
+          <Header>Login Here</Header>
+          <StyledInput
+            type="text"
+            placeholder="Username"
+            // value={username}
+            // onChange={e => setUserName(e.target.value)}
+            required
+          />
+          <StyledInput
+            type="password"
+            placeholder="password"
+            // value={password}
+            // onChange={e => setPassword(e.target.value)}
+            required
+          />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
   )
 }
 
