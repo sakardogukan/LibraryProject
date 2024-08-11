0f8d88bf33f75a226d617c89ab5d6a70df284133
DGKN <sakardogukan@gmail.com>
2024-08-11 18:10:07 +0300

    navbar.jsx file updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from "react"
+import { menuIcon } from "../../helper/iconData"
+import { Nav, HamburgerIcon, Menu, MenuLink, Logo } from "./Navbar.style"
+import { useAuthContext } from "../../context/AuthContext.jsx"
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+  const { user, setUser } = useAuthContext()
+
+  const logOut = () => {
+    setUser(false)
+  }
+  const handleClose = () => {
+    setToggle(false)
+  }
+
   return (
-    <div>Navbar</div>
+    <Nav justify="space-between" wrap="wrap">
+      <Logo>Library Project</Logo>
+      <HamburgerIcon onClick={() => setToggle(!toggle)}>
+        {menuIcon}
+      </HamburgerIcon>
+      <Menu showMenu={toggle}>
+        <MenuLink to="/" onClick={handleClose}>
+          Home
+        </MenuLink>
+        <MenuLink to="/about" onClick={handleClose}>
+          About
+        </MenuLink>
+
+        {user ? (
+          <MenuLink to="/login" onClick={logOut}>
+            Logout
+          </MenuLink>
+        ) : (
+          <>
+            <MenuLink to="/login" onClick={handleClose}>
+              Login
+            </MenuLink>
+            <MenuLink to="/register" onClick={handleClose}>
+              Register
+            </MenuLink>
+          </>
+        )}
+      </Menu>
+    </Nav>
   )
 }
 
