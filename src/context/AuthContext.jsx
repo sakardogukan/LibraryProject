7d5cae45402dbf65a5a5e2fc7a348471f8f9d2e4
DGKN <sakardogukan@gmail.com>
2024-08-11 18:08:51 +0300

    AuthContext file updated

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,1 +1,22 @@
 // Auth Context
+
+import { createContext, useContext, useState } from "react"
+
+//! 1- Theme Context oluşturulması:
+const AuthContext = createContext()
+
+//! 2- Sarmalayıcı (Provider) Componenet:
+const AuthContextProvider = ({ children }) => {
+  // local State
+  const [user, setUser] = useState(false)
+  const values = { user, setUser }
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
+}
+
+//! 3- Consuming custom hook:
+export const useAuthContext = () => {
+  return useContext(AuthContext)
+}
+
+export default AuthContextProvider
